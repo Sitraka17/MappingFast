3edcd116bc5df5f45acd00c07266f774016e3117
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2024-06-23 20:36:38 +0000

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,46 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import osmnx as ox
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-"""
-# Welcome to Streamlit!
+# Streamlit app
+st.title("City Map Generator using OSMnx")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Input for the city name
+city_name = st.text_input("Enter the city name:", "")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if st.button("Make"):
+    if city_name:
+        place = [city_name]
+        gdf_nodes = gdf_edges = None
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+        for place in place:
+            G = ox.graph_from_place(place, retain_all=True, simplify=True, network_type='walk')
+            n_, e_ = ox.graph_to_gdfs(G)
+            n_["place"] = place
+            e_["place"] = place
+            if gdf_nodes is None:
+                gdf_nodes = n_
+                gdf_edges = e_
+            else:
+                gdf_nodes = pd.concat([gdf_nodes, n_])
+                gdf_edges = pd.concat([gdf_edges, e_])
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+        # Plot it
+        colors = {city_name: 'blue'}
+        fig, ax = plt.subplots(figsize=(10, 10))
+        gdf_edges.plot(ax=ax, column="place", cmap=ListedColormap([colors[k] for k in sorted(colors.keys())]), alpha=1, linewidth=2, edgecolor='blue', facecolor='orange')
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+        plt.title(city_name)
+        plt.xticks([])
+        plt.yticks([])
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+        st.pyplot(fig)
+    else:
+        st.warning("Please enter a city name.")
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
